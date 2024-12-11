1cab0c4b8304aafe01155e1b3128f4bdc0515542
Jack Bennetto <jack.bennetto@slalom.com>
2024-12-11 10:55:30 -0800

    actually handle info; adding --verbose; use argparse; improve README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ai_grapher
-A simple GenAI application to graph data
+A simple command-line GenAI application to graph data
+
+To use, run
+
+```sh
+python grapher.py [-v] <csv file>
+```
+
+where `<csv file>` is the name of a csv file with the feature names in the first row (so it can be read by `pd.read_csv()`) The repo includes `iris.csv` as an example.
+
+It will then ask you what to graph; reply in English (e.g., "graph petal length vs width") and pop up a graph. After closing the graph you can add additional details (e.g., "plot the species in different colors", "make the dots larger and more transparent")
+
+Use the `-v` flag to get additional output, including the code it is executing.
+
+**WARNING: This contains no guardrails and will run GenAI-created code directly on your system. Running it outside of a container may have dire consequences, including data loss, particularly if you ask it do delete things.**
--- grapher.py
+++ grapher.py
@@ -1,13 +1,15 @@
-import sys
 import os
+import io
+import argparse
 import logging
-logging.getLogger().setLevel(logging.INFO)
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import pandas as pd
 import openai
+
 
 class Grapher:
 
@@ -24,7 +26,6 @@ class Grapher:
     The python code should not affect the computer in any other way, including reading, writing, modifying, or deleting files.
     Question: '''{}'''"""
 
-
     prompt_fix = """I have a pandas DataFrame called `df`.
     The results of `df.info()` are ```{}```.
     Produce python code based on the following Question to produce a graph with the matplotlib library.
@@ -37,10 +38,15 @@ class Grapher:
     Error ```{}```
     """
 
-
     def __init__(self, filename):
         self.df = pd.read_csv(filename)
-        self.info = self.df.info()
+        buffer = io.StringIO()
+        self.df.info(buf=buffer)
+
+        self.info = buffer.getvalue()
+
+        logging.info("info() from dataframe")
+        logging.info(self.info)
         self.max_error_count = 3
 
         if "OPENAI_API_KEY" in os.environ:
@@ -49,21 +55,17 @@ class Grapher:
             logging.error("Environment variable `OPEN_API_KEY` must be set")
             exit()
 
-        self.client = openai.OpenAI(
-            api_key=api_key)
+        self.client = openai.OpenAI(api_key=api_key)
         self.questions = []
 
-
-    
     def call(self, prompt):
         response = self.client.chat.completions.create(
-                messages=[
+            messages=[
                 {"role": "user", "content": prompt},
             ],
             model="gpt-4o-mini",
         )
         return response.choices[0].message.content
-
 
     def check_graphability(self, question):
         prompt = self.prompt_check.format(self.info, question)
@@ -79,12 +81,12 @@ class Grapher:
         code = self.call(prompt)
 
         return code
-    
+
     def execute(self, question):
         self.questions.append(question)
 
         question = "\n".join(self.questions)
-        if self.check_graphability(question) or True:
+        if self.check_graphability(question):
             code = grapher.generate_graph_code(question)
 
             # This is (should be) used in exec of genai-generated code
@@ -106,42 +108,53 @@ class Grapher:
                     code = self.call(prompt)
                     logging.info(code)
 
-            else: # NB: for/else clause
+            else:  # NB: for/else clause
                 print("I'm having trouble turning that into code.")
 
         else:
             print("I can't make a graph out of that.")
-        
+
     def main(self):
         while True:
-            question = input("What can I graph for you? ")
-            if not question.strip():
-                print(grapher.questions)
+            question = input("What can I graph for you?  ")
+            if not question.strip() or question.lower() in ("q", "quit", "x", "exit"):
+                logging.info(f"Questions so far:\n {"\n".join(grapher.questions)}")
                 break
             self.execute(question)
-            print("Thanks for graphing stuff with me!")
 
+        print("Thanks for graphing stuff with me!")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("grapher is a program to use GenAI to make graphs from data")
-        print()
-        print("Usage:")
-        print("  python grapher.py <filename.csv>")
-        print()
-        print("<filename.csv> must be a csv file; the first row is expected to contain feature names.")
-        print()
-        print("WARNING: This contains no guardrails and will run GenAI-created code directly on your")
-        print("system. Running it outside of a container may have dire consequences, including data loss,.")
-        print("particularly if you ask it do delete things.")
-        exit()
-    
-    filename = sys.argv[1]
 
-    grapher = Grapher(filename)
+    parser = argparse.ArgumentParser(
+        prog="grapher",
+        description="grapher is a program to use GenAI to make graphs from data",
+        epilog="""
+grapher is a program to use GenAI to make graphs from data
+
+Usage:
+  python grapher.py <filename.csv>
+<filename.csv> must be a csv file; the first row is expected to contain feature names.
+
+WARNING: This contains no guardrails and will run GenAI-created code directly on your
+system. Running it outside of a container may have dire consequences, including data loss,
+particularly if you ask it do delete things.
+                    """,
+    )
+    parser.add_argument(
+        "filename",
+        type=str,
+        help="The csv file to graph; the first row is expected to contain feature names.",
+    )
+
+    parser.add_argument("-v", "--verbose", action="store_true", help="Print ")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+    grapher = Grapher(args.filename)
     grapher.main()
-
-
-
-    
